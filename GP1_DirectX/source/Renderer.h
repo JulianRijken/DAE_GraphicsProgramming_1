cebca583c79712458e0069b1de62b992a3ac32c9
Julian <rijkenjulian@gmail.com>
2023-12-05 23:18:54 +0100

    Finished HW Rasterizer initialization

--- GP1_DirectX/source/Renderer.h
+++ GP1_DirectX/source/Renderer.h
@@ -22,13 +22,23 @@ namespace dae
 	private:
 		SDL_Window* m_pWindow{};
 
-		int m_Width{};
-		int m_Height{};
+		int m_WindowWidth{};
+		int m_WindowHeight{};
 
 		bool m_IsInitialized{ false };
 
+		inline static constexpr float SCREEN_CLEAR_COLOR[4]{ 0.0f,0.0f,0.3f,1.0f };
+
 		//DIRECTX
 		HRESULT InitializeDirectX();
-		//...
+
+		ID3D11Device*			m_DevicePtr{};
+		ID3D11DeviceContext*	m_DeviceContextPtr{};
+		IDXGISwapChain*			m_SwapChainPtr{};
+		ID3D11Texture2D*		m_DepthStencilBufferPtr{};
+		ID3D11DepthStencilView* m_DepthStencilViewPtr{};
+		ID3D11Resource*			m_RenderTargetBufferPtr{};
+		ID3D11RenderTargetView* m_RenderTargetViewPtr{};
+
 	};
 }
